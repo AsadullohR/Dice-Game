ba2ae0464e53e954899cd3ea7e7dc50c4fb53b02
Asadullo Rakhimov <58390694+AsadullohR@users.noreply.github.com>
2020-09-21 21:19:33 +0200

    Dice Game

--- index.js
+++ index.js
@@ -1,40 +1,19 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var radomDiceImage1 = "dice" + randomNumber1 + ".png";
+var randomImageSource1 = "images/" + radomDiceImage1;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource1);
+
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var diceNum1 = randomNumber1;
-var diceNum2 = randomNumber2;
+var radomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + radomDiceImage2;
+var image2 = document.querySelectorAll("img")[0];
+image2.setAttribute("src", randomImageSource2);
 
-if (diceNum1 === 1) {
-  document.querySelector(".img1").src = "images/dice1.png";
-} else if (diceNum1 === 2) {
-  document.querySelector(".img1").src = "images/dice2.png";
-} else if (diceNum1 === 3) {
-  document.querySelector(".img1").src = "images/dice3.png";
-} else if (diceNum1 === 4) {
-  document.querySelector(".img1").src = "images/dice4.png";
-} else if (diceNum1 === 5) {
-  document.querySelector(".img1").src = "images/dice5.png";
-} else if (diceNum1 === 6) {
-  document.querySelector(".img1").src = "images/dice6.png";
-}
-
-if (diceNum2 === 1) {
-  document.querySelector(".img2").src = "images/dice1.png";
-}else if (diceNum2 === 2) {
-  document.querySelector(".img2").src = "images/dice2.png";
-}else if (diceNum2 === 3) {
-  document.querySelector(".img2").src = "images/dice3.png";
-}else if (diceNum2 === 4) {
-  document.querySelector(".img2").src = "images/dice4.png";
-}else if (diceNum2 === 5) {
-  document.querySelector(".img2").src = "images/dice5.png";
-}else if (diceNum2 === 6) {
-  document.querySelector(".img2").src = "images/dice6.png";
-}
-
-if(diceNum1 > diceNum2){  // Player 1 wins
+if (randomNumber1 > randomNumber2) { // Player 1 wins
   document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!"
-}else if(diceNum2 > diceNum1){ // Player 2 wins
+} else if (randomNumber2 > randomNumber1) { // Player 2 wins
   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩"
-}else{  // Draw
+} else { // Draw
   document.querySelector("h1").innerHTML = "Draw!"
 }
